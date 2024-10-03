d5a7e3edfe95646dca47124ab764a71cf0cf1c7f
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-10-03 15:01:06 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -4,149 +4,182 @@ const path = require("path");
 const sharp = require("sharp");
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand(
-        "extension.bulkResizeImages",
-        async (uri) => {
-            // Afficher une boîte de dialogue pour demander la taille du côté le plus long
-            const sizeInput = await vscode.window.showInputBox({
-                prompt: "Entrez la taille du côté le plus long (en pixels)",
-                placeHolder: "Ex. 800",
-                validateInput: (value) => {
-                    if (!value.match(/^\d+$/)) {
-                        return "La valeur doit être un nombre entier.";
-                    }
-                    return null;
-                },
-            });
+	let disposable = vscode.commands.registerCommand(
+		"extension.bulkResizeImages",
+		async (uri) => {
+			// Afficher une boîte de dialogue pour demander la taille du côté le plus long
+			const sizeInput = await vscode.window.showInputBox({
+				prompt: "Entrez la taille du côté le plus long (en pixels)",
+				placeHolder: "Ex. 800",
+				validateInput: (value) => {
+					if (!value.match(/^\d+$/)) {
+						return "La valeur doit être un nombre entier.";
+					}
+					return null;
+				},
+			});
 
-            if (!sizeInput) {
-                return; // L'utilisateur a annulé l'opération
-            }
+			if (!sizeInput) {
+				return; // L'utilisateur a annulé l'opération
+			}
 
-            const size = parseInt(sizeInput);
-            let filePaths = [];
+			const size = parseInt(sizeInput);
 
-            if (uri && uri.fsPath) {
-                // Si un chemin est sélectionné dans l'explorateur de fichiers, utilisez-le
-                filePaths.push(uri.fsPath);
-            } else {
-                // Sinon, afficher un dialogue pour sélectionner un fichier ou un dossier
-                const result = await vscode.window.showOpenDialog({
-                    canSelectFiles: true,
-                    canSelectFolders: true,
-                    canSelectMany: true,
-                    openLabel: "Sélectionner",
-                });
+			let filePaths = [];
 
-                if (!result || result.length === 0) {
-                    vscode.window.showErrorMessage("Aucun fichier ou dossier sélectionné.");
-                    return;
-                }
+			if (uri && uri.fsPath) {
+				// Si un chemin est sélectionné dans l'explorateur de fichiers, utilisez-le
+				filePaths.push(uri.fsPath);
+			} else {
+				// Sinon, afficher un dialogue pour sélectionner un fichier ou un dossier
+				const result = await vscode.window.showOpenDialog({
+					canSelectFiles: true,
+					canSelectFolders: true,
+					canSelectMany: true,
+					openLabel: "Sélectionner",
+				});
 
-                // Utiliser les chemins des fichiers ou dossiers sélectionnés
-                filePaths = result.map((file) => file.fsPath);
-            }
+				if (!result || result.length === 0) {
+					vscode.window.showErrorMessage(
+						"Aucun fichier ou dossier sélectionné."
+					);
+					return;
+				}
 
-            try {
-                // Redimensionner toutes les images sélectionnées
-                await Promise.all(
-                    filePaths.map(async (filePath) => {
-                        const stat = await fs.promises.stat(filePath);
+				// Utiliser les chemins des fichiers ou dossiers sélectionnés
+				filePaths = result.map((file) => file.fsPath);
+			}
 
-                        if (stat.isFile()) {
-                            // Si c'est un fichier, redimensionnez-le directement
-                            await resizeImage(filePath, size);
-                        } else if (stat.isDirectory()) {
-                            // Si c'est un dossier, traiter le dossier et les sous-dossiers
-                            await processDirectory(filePath, size);
-                        }
-                    })
-                );
-            } catch (error) {
-                vscode.window.showErrorMessage(
-                    `Une erreur s'est produite lors du redimensionnement des images : ${error}`
-                );
-            }
-        }
-    );
+			try {
+				// Redimensionner toutes les images sélectionnées
+				await Promise.all(
+					filePaths.map(async (filePath) => {
+						const stat = await fs.promises.stat(filePath);
 
-    context.subscriptions.push(disposable);
-}
+						if (stat.isFile()) {
+							// Si c'est un fichier, redimensionnez-le directement
+							await resizeImage(filePath, size);
+						} else if (stat.isDirectory()) {
+							// Si c'est un dossier, redimensionnez toutes les images à l'intérieur
+							const files = await fs.promises.readdir(filePath);
+							const imageFiles = files.filter((fileName) =>
+								[
+									".png",
+									".jpg",
+									".jpeg",
+									".webp",
+									".tiff",
+									".heic",
+									".PNG",
+									".JPG",
+									".JPEG",
+									".WEBP",
+									".TIFF",
+									".HEIC",
+								].includes(path.extname(fileName).toLowerCase())
+							);
 
-// Fonction pour traiter un répertoire et ses sous-répertoires
-async function processDirectory(directoryPath, requestedSize) {
-    const files = await fs.promises.readdir(directoryPath);
+							await Promise.all(
+								imageFiles.map(async (fileName) => {
+									const imagePath = path.join(
+										filePath,
+										fileName
+									);
+									await resizeImage(imagePath, size);
+								})
+							);
+						}
+					})
+				);
+			} catch (error) {
+				vscode.window.showErrorMessage(
+					`Une erreur s'est produite lors du redimensionnement des images : ${error}`
+				);
+			}
+		}
+	);
 
-    await Promise.all(
-        files.map(async (fileName) => {
-            const filePath = path.join(directoryPath, fileName);
-            const stat = await fs.promises.stat(filePath);
-
-            if (stat.isFile()) {
-                await resizeImage(filePath, requestedSize);
-            } else if (stat.isDirectory()) {
-                await processDirectory(filePath, requestedSize); // Appel récursif pour les sous-dossiers
-            }
-        })
-    );
+	context.subscriptions.push(disposable);
 }
 
 async function resizeImage(filePath, requestedSize) {
-    const extname = path.extname(filePath).toLowerCase();
-    if (![
-        ".png", ".jpg", ".jpeg", ".webp", ".tiff", ".heic",
-        ".PNG", ".JPG", ".JPEG", ".WEBP", ".TIFF", ".HEIC"
-    ].includes(extname)) {
-        vscode.window.showWarningMessage(
-            `Le fichier ${path.basename(filePath)} n'est pas une image valide (.png, .jpg, .jpeg). Il ne sera pas redimensionné.`
-        );
-        return;
-    }
+	const extname = path.extname(filePath).toLowerCase();
+	if (
+		![
+			".png",
+			".jpg",
+			".jpeg",
+			".webp",
+			".tiff",
+			".heic",
+			".PNG",
+			".JPG",
+			".JPEG",
+			".WEBP",
+			".TIFF",
+			".HEIC",
+		].includes(extname)
+	) {
+		vscode.window.showWarningMessage(
+			`Le fichier ${path.basename(
+				filePath
+			)} n'est pas une image valide (.png, .jpg, .jpeg). Il ne sera pas redimensionné.`
+		);
+		return;
+	}
 
-    try {
-        const imageBuffer = await fs.promises.readFile(filePath);
-        const imageMetadata = await sharp(imageBuffer).metadata();
+	try {
+		const imageBuffer = await fs.promises.readFile(filePath);
+		const imageMetadata = await sharp(imageBuffer).metadata();
 
-        // Vérifier si la taille demandée est supérieure à la taille de l'image
-        const maxSize = Math.max(imageMetadata.width, imageMetadata.height);
-        if (requestedSize > maxSize) {
-            vscode.window.showWarningMessage(
-                `La taille demandée est supérieure à la taille maximale de l'image. L'image ne sera pas redimensionnée.`
-            );
-            return;
-        }
+		// Vérifier si la taille demandée est supérieure à la taille de l'image
+		const maxSize = Math.max(imageMetadata.width, imageMetadata.height);
+		if (requestedSize > maxSize) {
+			vscode.window.showWarningMessage(
+				`La taille demandée est supérieure à la taille maximale de l'image. L'image ne sera pas redimensionnée.`
+			);
+			return;
+		}
 
-        const resizedImageBuffer = await sharp(imageBuffer)
-            .resize({
-                width: requestedSize,
-                height: requestedSize,
-                fit: "inside",
-            })
-            .toBuffer();
+		const resizedImageBuffer = await sharp(imageBuffer)
+			.resize({
+				width: requestedSize,
+				height: requestedSize,
+				fit: "inside",
+			})
+			.toBuffer();
 
-        const resizedFileName = path.basename(filePath).replace(
-            /\.(png|jpg|jpeg|webp|tiff|heic)$/,
-            `_resized_${requestedSize}x${requestedSize}.$1`
-        );
-        const outputPath = path.join(path.dirname(filePath), resizedFileName);
+		const resizedFileName = path
+			.basename(filePath)
+			.replace(
+				/\.(png|jpg|jpeg|webp|tiff|heic|PNG|JPG|JPEG|WEBP|TIFF|HEIC)$/,
+				`_resized_${requestedSize}x${requestedSize}.$1`
+			);
+		const outputPath = path.join(path.dirname(filePath), resizedFileName);
 
-        await fs.promises.writeFile(outputPath, resizedImageBuffer);
+		await fs.promises.writeFile(outputPath, resizedImageBuffer);
 
-        vscode.window.showInformationMessage(
-            `L'image ${path.basename(filePath)} a été redimensionnée et enregistrée sous ${path.basename(outputPath)}`
-        );
-    } catch (error) {
-        vscode.window.showErrorMessage(
-            `Une erreur s'est produite lors du redimensionnement de l'image ${path.basename(filePath)} : ${error}`
-        );
-    }
+		vscode.window.showInformationMessage(
+			`L'image ${path.basename(
+				filePath
+			)} a été redimensionnée et enregistrée sous ${path.basename(
+				outputPath
+			)}`
+		);
+	} catch (error) {
+		vscode.window.showErrorMessage(
+			`Une erreur s'est produite lors du redimensionnement de l'image ${path.basename(
+				filePath
+			)} : ${error}`
+		);
+	}
 }
 
 function deactivate() {
-    // Clean up resources here if necessary
+	// Clean up resources here if necessary
 }
 
 module.exports = {
-    activate,
-    deactivate,
+	activate,
+	deactivate,
 };
